55431c8127ba886eca2d528c90f8aaea48eee9cb
Lau Llobet <lau.llobet@gmail.com>
2018-05-17 11:49:47 +0200

    Ticket Item updadets its own quality depending on the sellin

--- Java/src/main/java/com/gildedrose/TicketsItem.java
+++ Java/src/main/java/com/gildedrose/TicketsItem.java
@@ -1,22 +1,35 @@
 package com.gildedrose;
 
 public class TicketsItem extends Item {
+
+    public static final int MAXIMUM_FOR_MODERATE_SELLIN = 11;
+    public static final int OUTSTANDING_QUALITY = 50;
+    public static final int MINIMUM_FOR_MODERATE_SELLING = 6;
+
     public TicketsItem(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
     }
 
     @Override
     public void updateQuality() {
-        if (sellIn < 11 && sellIn >= 6 && hasNotOutstandingQuality()) {
+        if (hasModerateSellin() && hasNotOutstandingQuality()) {
             quality = quality + 1;
             return;
         }
-        if (sellIn < 6 && hasNotOutstandingQuality()) {
+        if (hasLowSellin() && hasNotOutstandingQuality()) {
             quality = quality + 2;
         }
     }
 
+    private boolean hasLowSellin() {
+        return sellIn < MINIMUM_FOR_MODERATE_SELLING;
+    }
+
+    private boolean hasModerateSellin() {
+        return sellIn < MAXIMUM_FOR_MODERATE_SELLIN && sellIn >= 6;
+    }
+
     private boolean hasNotOutstandingQuality() {
-        return quality < 50;
+        return quality < OUTSTANDING_QUALITY;
     }
 }
