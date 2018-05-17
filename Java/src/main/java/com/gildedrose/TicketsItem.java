71bee4b1a2542ffd689e6bf3467f20d4089a03db
Lau Llobet <lau.llobet@gmail.com>
2018-05-17 15:07:06 +0200

    Quality update dependence on sellin is now modeled without ifs

--- Java/src/main/java/com/gildedrose/TicketsItem.java
+++ Java/src/main/java/com/gildedrose/TicketsItem.java
@@ -1,9 +1,16 @@
 package com.gildedrose;
 
+import java.util.Hashtable;
+
 public class TicketsItem extends Item {
 
     public static final int OUTSTANDING_QUALITY = 50;
     private final Sellin sellInObj;
+    private final Hashtable<String,Integer> incrementForGiven = new Hashtable<String,Integer>(){{
+        put(Sellin.LOW,2);
+        put(Sellin.MODERATE,1);
+        put(Sellin.HIGH,0);
+    }};
 
     public TicketsItem(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
@@ -15,13 +22,7 @@ public class TicketsItem extends Item {
         if(hasOutstandingQuality()){
             return;
         }
-        if (sellInObj.getType().equals(Sellin.MODERATE) ){
-            quality = quality + 1;
-            return;
-        }
-        if (sellInObj.getType().equals(Sellin.LOW) ){
-            quality = quality + 2;
-        }
+        quality = quality +  incrementForGiven.get(sellInObj.getType());
     }
 
     private boolean hasOutstandingQuality() {
