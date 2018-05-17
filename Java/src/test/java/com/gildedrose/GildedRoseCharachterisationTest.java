d2b82027d2cc9400b58d1f75056ad09349ed59ab
Lau Llobet <lau.llobet@gmail.com>
2018-05-17 10:32:32 +0200

    interpolation tests setted up as characterization

--- Java/src/test/java/com/gildedrose/GildedRoseCharachterisationTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseCharachterisationTest.java
@@ -2,24 +2,70 @@ package com.gildedrose;
 
 import static org.junit.Assert.*;
 
+import junitparams.JUnitParamsRunner;
+import junitparams.Parameters;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+@RunWith(JUnitParamsRunner.class)
 public class GildedRoseCharachterisationTest {
 
     @Test
-    public void random_name_losses_2_quality_with_0_as_limit() {
-        Item[] items = new Item[] { new Item("foo", 0, 50) };
+    @Parameters({"0, 0",
+            "1, 0",
+            "2, 0",
+            "3, 1",
+            "4, 2",
+            "20,18"})
+    public void random_name_losses_2_quality_with_0_as_limit(int initialQuality, int expectedQuality) {
+        Item[] items = new Item[] { new Item("foo", 0, initialQuality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(48, app.items[0].quality);
+        assertEquals(expectedQuality, app.items[0].quality);
     }
 
     @Test
-    public void aged_brie_rises_quality_by_2_till_50() {
-        Item[] items = new Item[] { new Item("Aged Brie", 0, 32) };
+    @Parameters({"0, 2",
+            "1, 3",
+            "2, 4",
+            "48, 50",
+            "49, 50",
+            "50, 50"})
+    public void aged_brie_rises_quality_by_2_till_50(int intialQuality, int expectedQuality) {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, intialQuality) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals(34, app.items[0].quality);
+        assertEquals(expectedQuality, app.items[0].quality);
+    }
+
+    @Test
+    @Parameters({"0, 0",
+            "100, 100",
+            "40, 40",
+            "37, 37",
+            "36, 36",
+            "2,2"})
+    public void sulfuras_dont_change_quality_regadless_any_previous_quality(int initialQuality, int expectedQuality) {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, initialQuality) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(expectedQuality, app.items[0].quality);
+    }
+
+    @Test
+    @Parameters({"7, 8, 11",
+            "7, 8, 50",
+            "7, 8, 60",
+            "30, 31, 50",
+            "49, 50, 50",
+
+            "50, 50, 50",
+            "70, 70, 50"})
+    public void backstage_passes_increment_quality_if_they_are_not_selled_in_huge_amounts(int initialQuality, int expectedQuality, int sellin) {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", sellin, initialQuality) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(expectedQuality, app.items[0].quality);
     }
 
 }
